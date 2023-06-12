a1cd30306fc76a0aec2e2b040e0dcf425193c422
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-06-12 16:11:30 -0600

    update model

--- lora_example.py
+++ lora_example.py
@@ -6,9 +6,9 @@ import torch.optim as optim
 class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(10, 20)
-        self.fc2 = nn.Linear(20, 15)
-        self.fc3 = nn.Linear(15, 1)
+        self.fc1 = nn.Linear(100, 200)  # Increase the input size to 100 and the first layer size to 200
+        self.fc2 = nn.Linear(200, 150)  # Increase the second layer size to 150
+        self.fc3 = nn.Linear(150, 1)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
@@ -20,9 +20,9 @@ model = SimpleNN()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 criterion = nn.BCELoss()
 
-for epoch in range(100):
-    inputs = torch.randn(32, 10)
-    targets = torch.randint(2, (32, 1)).float()
+for epoch in range(1000):  # Increase the number of epochs to 1000
+    inputs = torch.randn(320, 100)  # Increase the batch size to 320 and input size to 100
+    targets = torch.randint(2, (320, 1)).float()
     
     optimizer.zero_grad()
     outputs = model(inputs)
@@ -30,15 +30,15 @@ for epoch in range(100):
     loss.backward()
     optimizer.step()
     
-    if (epoch+1) % 10 == 0:
-        print(f'Epoch {epoch+1}/{100}, Loss: {loss.item()}')
+    if (epoch+1) % 100 == 0:
+        print(f'Epoch {epoch+1}/{1000}, Loss: {loss.item()}')
 
 # 2. LoRA method applied to random subspaces of the model weights
 optimizer_lora_random = optim.SGD([model.fc1.weight], lr=0.01)
 
-for epoch in range(50):
-    inputs = torch.randn(32, 10)
-    targets = torch.randint(2, (32, 1)).float()
+for epoch in range(500):  # Increase the number of epochs to 500
+    inputs = torch.randn(320, 100)  # Increase the batch size to 320 and input size to 100
+    targets = torch.randint(2, (320, 1)).float()
     
     optimizer_lora_random.zero_grad()
     outputs = model(inputs)
@@ -46,17 +46,16 @@ for epoch in range(50):
     loss.backward()
     optimizer_lora_random.step()
     
-    if (epoch+1) % 10 == 0:
-        print(f'Fine-tuning (random) Epoch {epoch+1}/{50}, Loss: {loss.item()}')
+    if (epoch+1) % 50 == 0:
+        print(f'Fine-tuning (random) Epoch {epoch+1}/{500}, Loss: {loss.item()}')
 
 # 3. LoRA method applied to strongly activated regions of the model weights
-# Define a mask for the first layer weights based on the weight magnitude
 with torch.no_grad():
     mask = (model.fc1.weight > model.fc1.weight.mean()).float()
 
-for epoch in range(50):
-    inputs = torch.randn(32, 10)
-    targets = torch.randint(2, (32, 1)).float()
+for epoch in range(500):  # Increase the number of epochs to 500
+    inputs = torch.randn(320, 100)  # Increase the batch size to 320 and input size to 100
+    targets = torch.randint(2, (320, 1)).float()
     
     optimizer.zero_grad()
     outputs = model(inputs)
@@ -66,6 +65,6 @@ for epoch in range(50):
         model.fc1.weight.grad *= mask
     optimizer.step()
     
-    if (epoch+1) % 10 == 0:
-        print(f'Fine-tuning (strong activation) Epoch {epoch+1}/{50}, Loss: {loss.item()}')
+    if (epoch+1) % 50 == 0:
+        print(f'Fine-tuning (strong activation) Epoch {epoch+1}/{500}, Loss: {loss.item()}')
 
